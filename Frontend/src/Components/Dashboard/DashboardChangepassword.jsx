d010b90abc71ba137ddf9b917340c34ddbdc8965
Vishnu.C P <115421837+vishnu-13-97@users.noreply.github.com>
2025-01-18 21:56:15 +0530

    Update DashboardChangepassword.jsx

--- Frontend/src/Components/Dashboard/DashboardChangepassword.jsx
+++ Frontend/src/Components/Dashboard/DashboardChangepassword.jsx
@@ -28,7 +28,7 @@ function DashboardChangepassword() {
     setSuccessMessage('');
 
     try {
-      const response = await axios.post('https://scope-project-backend.onrender.com/dashboard/changepassword', {
+      const response = await axios.post('https://scope-project-backend.onrender.com', {
         currentPassword: formData.currentPassword,
         newPassword: formData.newPassword,
         confirmPassword: formData.confirmPassword
