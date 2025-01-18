dfd23fd79c34eaca26f6d0a826cde46fb5b646e5
Vishnu.C P <115421837+vishnu-13-97@users.noreply.github.com>
2025-01-18 21:56:40 +0530

    Update DashboardHome.jsx

--- Frontend/src/Components/Dashboard/DashboardHome.jsx
+++ Frontend/src/Components/Dashboard/DashboardHome.jsx
@@ -15,7 +15,7 @@ function DashboardHome() {
   }
 
   axios
-    .get('https://scope-project-backend.onrender.com/dashboard', {
+    .get('https://scope-project-backend.onrender.com', {
       withCredentials: true,
       headers: {
         'Authorization': `Bearer ${token}`, // Include the token in the headers
