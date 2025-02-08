0804ec6803c1a4918eac25b0769a99c846eab742
Vishnu.C P <115421837+vishnu-13-97@users.noreply.github.com>
2025-02-08 23:48:21 +0530

    Update DashboardSidebar.jsx

--- Frontend/src/Components/Dashboard/DashboardSidebar.jsx
+++ Frontend/src/Components/Dashboard/DashboardSidebar.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 function DashboardSidebar() {
- 
+  const navigate = useNavigate();
+
   const handleLogout = async (e) => {
     e.preventDefault();
 
-
     try {
       const response = await axios.post('https://scope-project-backend.onrender.com/dashboard/logout', {}, { withCredentials: true, timeout: 5000 });
-      
+
       if (response.data.message === 'Logged out successfully') {
- 
-  
-         window.location.href = '/login'
+        navigate('/login');
       } else {
-        console.error('Logout response was unexpected:', response.data);
+        console.error('Unexpected logout response:', response.data);
       }
     } catch (error) {
       console.error('Error logging out:', error);
@@ -25,14 +22,13 @@ function DashboardSidebar() {
   return (
     <div id="sidebar">
       <div className="sidebar-header">
-      <a href="/" className="d-flex align-items-center text-decoration-none">
-  <img src={'../../../assets/scope Images/scope-india-logo-bird.png'} alt="Scope India Logo" style={{"backgroundColor":"#051a69"}} className="img-fluid  rounded-circle w-25 mr-2" />
-  <span className="ms-2">Scope India</span>
-</a>
-
+        <a href="/" className="d-flex align-items-center text-decoration-none">
+          <img src={'../../../assets/scope Images/scope-india-logo-bird.png'} alt="Scope India Logo" style={{ "backgroundColor": "#051a69" }} className="img-fluid rounded-circle w-25 mr-2" />
+          <span className="ms-2">Scope India</span>
+        </a>
       </div>
       <ul className="list-unstyled component m-0 mt-4">
-      <li className="active">
+        <li className="active">
           <a href="/dashboard/" className="dashboard">
             <i className="material-icons">account_circle</i>Profile
           </a>
